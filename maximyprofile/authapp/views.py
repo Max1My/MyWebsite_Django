eee4ae475f420f226c7a3de88370ff1512346dea
MaxiMy <maximy.pro@gmail.com>
2022-02-26 23:19:33 +0300

    edited

--- maximyprofile/authapp/views.py
+++ maximyprofile/authapp/views.py
@@ -74,7 +74,7 @@ class RegisterListView(FormView, BaseClassContextMixin):
 
 
 class Logout(LogoutView):
-    success_url = reverse_lazy('mainapp:main')
+    success_url = reverse_lazy('mainapp:index')
 
 class ProfileFormView(UpdateView,BaseClassContextMixin,UserDispatchMixin):
     template_name = 'authapp/profile.html'
